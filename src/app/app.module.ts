f5ec619c55376445d78f4db52c4471be0454586d
Fredrik <itla.main@gmail.com>
2017-04-11 22:26:36 +0200

    first assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './messages/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './messages/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule,
